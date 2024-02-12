c7386068e69b636d42755b18850be5a6b7e9df09
pangpond <pondzz@gmail.com>
2024-02-12 17:02:54 +0700

    add refactoring report controller

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -13,141 +13,6 @@ class ReportController extends Controller
   public $cssInline;
   public $download = false;
 
-  public function actionPdf()
-  {
-
-    $params = Yii::$app->params;
-    $fontDirs = $params['defaultConfig']['fontDir'];
-    $fontData = $params['defaultFontConfig']['fontdata'];
-
-    $stylesheet = file_get_contents(Yii::getAlias('@webroot/css/kv-mpdf-bootstrap-sarabun.min.css'));
-    $stylesheetPdf = file_get_contents(Yii::getAlias('@webroot/css/pdf-sarabun.css'));
-
-
-    // get your HTML raw content without any layouts or scripts
-    $content = $this->renderPartial('pdf/template');
-
-    // setup kartik\mpdf\Pdf component
-    $pdf = new Pdf([
-      // set to use core fonts only
-      'mode' => Pdf::MODE_UTF8,
-      // A4 paper format
-      'format' => Pdf::FORMAT_A4,
-      // portrait orientation
-      'orientation' => Pdf::ORIENT_PORTRAIT,
-      // stream to browser inline
-      'destination' => Pdf::DEST_BROWSER,
-      // your html content input
-      'content' => $content,
-      'defaultFont' => 'thsarabun',
-      // format content from your own css file if needed or use the
-      // enhanced bootstrap css built by Krajee for mPDF formatting
-      'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/kv-mpdf-bootstrap.min.css',
-      // any css to be embedded if required
-      'cssInline' => '.kv-heading-1{font-size:18px}',
-
-      // set mPDF properties on the fly
-      'options' => [
-        'title' => 'Krajee Report Title',
-        'fontDir' => array_merge($fontDirs, [
-          Url::base() . 'fonts/',
-        ]),
-        'fontdata' => $fontData + [
-          'thsarabun' => $params['SetFontTHsarabun'],
-          'garuda' => $params['SetFontGaruda'],
-          'fontawesome' => $params['SetFontAwesome'],
-        ],
-
-      ],
-      // call mPDF methods on the fly
-      'methods' => [
-        'SetTitle' => 'Privacy Policy - Krajee.com',
-        'SetSubject' => 'Generating PDF files via yii2-mpdf extension has never been easy',
-        'SetHeader' => ['Krajee Privacy Policy||Generated On: ' . date("r")],
-        'SetFooter' => ['สร้างโดย||หน้า {PAGENO}'],
-        'SetAuthor' => 'Kartik Visweswaran',
-        'SetCreator' => 'Kartik Visweswaran',
-        'SetKeywords' => 'Krajee, Yii2, Export, PDF, MPDF, Output, Privacy, Policy, yii2-mpdf',
-        'WriteHTML' => [$stylesheet . $stylesheetPdf, 1]
-      ]
-    ]);
-
-
-    // $pdf->WriteHTML($stylesheet, 1);
-    // $pdf->WriteHTML($stylesheetPdf, 1);
-
-    // return the pdf output as per the destination setting
-    return $pdf->render();
-  }
-
-  public function actionMpdf()
-  {
-    $params = Yii::$app->params;
-    $fontDirs = $params['defaultConfig']['fontDir'];
-    $fontData = $params['defaultFontConfig']['fontdata'];
-
-    $filename = "ชื่อรายงาน";
-    $content = $this->renderPartial('pdf/template');
-
-    $orientation = Pdf::ORIENT_PORTRAIT;
-    $cssFilePath = "";
-    $watermark = "";
-    $paperFormat = Pdf::FORMAT_A4;
-
-    $mpdf = new \Mpdf\Mpdf([
-      'fontDir' => array_merge($fontDirs, [
-        Url::base() . 'fonts/',
-      ]),
-      'fontdata' => $fontData + ['thsarabun' => Yii::$app->params['SetFontTHsarabun']],
-      'default_font_size' => 18,
-      'default_font' => 'thsarabun',
-      'mode' => Pdf::MODE_UTF8,
-      'format' => $paperFormat,
-      'orientation' => $orientation,
-      'filename' => $filename . ".pdf",
-			'margin_top' => 14,
-			'margin_bottom' => 14,
-    ]);
- 
-    $header = $this->header($filename);
-    $footer = $this->footer();
-    $mpdf->SetTitle($filename);
-    $mpdf->SetHeader($header);
-    $mpdf->SetFooter($footer);
-
-    $stylesheet = file_get_contents(Yii::getAlias('@webroot/css/kv-mpdf-bootstrap-sarabun.min.css'));
-    $stylesheetPdf = file_get_contents(Yii::getAlias('@webroot/css/pdf-sarabun.css'));
-    $mpdf->WriteHTML($stylesheet, 1);
-    $mpdf->WriteHTML($stylesheetPdf, 1);
-
-    if (!empty($cssFilePath)) {
-      $customStylesheet = file_get_contents($cssFilePath);
-      $mpdf->WriteHTML($customStylesheet, 1);
-    }
-
-    if (!empty($watermark)) {
-      $mpdf->SetWatermarkImage($watermark, 0.1, [100, 100]);
-      $mpdf->showWatermarkImage = true;
-    }
-
-    $mpdf->showWatermarkText = true;
-
-    if ($this->cssInline) {
-      $mpdf->WriteHTML($this->cssInline, 1);
-    }
-
-    $mpdf->WriteHtml($content, 2);
-
-    if ($this->download) {
-      $mpdf->destination = Pdf::DEST_DOWNLOAD;
-    }
-
-    $filename = str_replace("/", "-", $filename);
-    $filename = str_replace(".", "_", $filename);
-
-    return $mpdf->Output($filename . ".pdf", "I");
-  }
-
   private function header($filename)
   {
     $header = array(
@@ -220,7 +85,7 @@ class ReportController extends Controller
 
     return $footer;
   }
-  
+
   private function outputMpdf($config, $data)
   {
     $params = Yii::$app->params;
@@ -302,47 +167,11 @@ class ReportController extends Controller
 
     $data = [
       'tax_id' => '0994000158149',
+      'firstname' => 'dddfdfdf',
+      'qr' => '099400026587500-13980-101-200000',
+      'codabar1' => '099400026587500-13980-101-200000',
     ];
 
     $this->outputMpdf($config, $data);
   }
-  
-  public function actionStickerPdf(array $selection)
-    {
-        $content = $this->renderPartial('_sticker', [
-            'selection' => $selection,
-        ]);
-
-        $pdf = new Pdf([
-            // set to use core fonts only
-            'mode' => Pdf::MODE_UTF8,
-            // A4 paper format
-            //'format' => Pdf::FORMAT_A4,
-            'format' => [25, 20],//Pdf::FORMAT_A4,
-            'marginLeft' => 1,
-            'marginRight' => 1,
-            'marginTop' => 1,
-            'marginBottom' => false,
-            'marginHeader' => false,
-            'marginFooter' => false,
-            // portrait orientation
-            'orientation' => Pdf::ORIENT_PORTRAIT,
-            // stream to browser inline
-            'destination' => Pdf::DEST_BROWSER,
-            // your html content input
-            'content' => $content,
-            // format content from your own css file if needed or use the
-            // enhanced bootstrap css built by Krajee for mPDF formatting
-            'cssFile' => '@frontend/web/css/kv-mpdf-bootstrap.css',
-            // any css to be embedded if required
-            'cssInline' => 'body{font-size:11px}',
-            // set mPDF properties on the fly
-            'options' => ['title' => 'Sticker'],
-            // call mPDF methods on the fly
-            'methods' => [
-                'SetHeader' => false,
-                'SetFooter' => false,
-            ]
-        ]);
-    }
 }
--- views/report/pdf/template.php
+++ views/report/pdf/template.php
@@ -1,178 +1,175 @@
 <body>
-<div class="row" style="margin-left: 0px;margin-right: 0px">
-    <div>
-        <div class="column3">
-            <img src="img/nikomwitthaya.png" alt="School icon" width="auto" height="80"/>
-        </div>
-
-        <div class="column4" style="padding-top: 5px">
-            <h5 style="line-height: 22px">
-                <b>ใบแจ้งการชำระเงินเพื่อบำรุงการศึกษา<br />
-                    โรงเรียนนิคมวิทยา</b>
-            </h5>
-            <hr style="height: 2px;border-width: 0;margin-top: -7px;margin-bottom: 5px;"/>
-            <b>
-                ภาคเรียนที่ <?php echo "2" ?>
-                ปีการศึกษา <?php echo "2566" ?></b>
-        </div>
-    </div>
-
-    <p style="float: right;width: 22%;padding: 5px;font-weight: bold;margin-top: -80px;text-align: center;border-width: 1px;border-style: solid;">
-        ส่วนที่ 1 (สำหรับนักเรียน)
-    </p>
-    <div class="row" style="margin-left: 0px; margin-right: 0px">
-        <div style="margin-left: 0px;margin-right: 0px;margin-top: 10px;float: left;width: 20%;">
-            ชื่อ-นามสกุลนักเรียน<br />
-            รหัสประจำตัวนักเรียน<br />
-            ชั้นห้องเรียน
-        </div>
+    <div class="row" style="margin-left: 0px;margin-right: 0px">
         <div>
-            <b>
-                <?php echo "เด็กชายกิตติธัช สนืทราษฎร์" ?><br />
-                <?php echo "13980" ?><br />
-                <?php echo "ม.1/1" ?><br /></b>
-        </div>
-
-        <div style="margin-top: 5px">
-            <table class="table table-bordered table-sm table-collapse" style="border: 1px solid">
-                <thead>
-                <tr>
-                    <th style="width: 450px;padding-top: 0px;vertical-align: middle;padding-bottom: 0px;background-color: #2dfeff;border: 1px solid;">
-                        <b>รายการ</b>
-                    </th>
-                    <th style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;background-color: #2dfeff;border: 1px solid;">
-                        <b>จำนวนเงิน</b>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td style="width: 450px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
-                        1. ค่าบำรุงการศึกษา
-                    </td>
-                    <td style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
-                        2,000.00
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 450px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;border: 1px solid;">
-                        <b>( สองพันบาทถ้วน )</b>
-                    </td>
-                    <td style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;background-color: #2dfeff;border: 1px solid;">
-                        <b>2,000.00</b>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="column5">
-            <h3>หมายเหตุ</h3>
-            <p style="font-size: 16px">
-                ช่องทางการชำระเงิน ดังนี้<br />
-                1. สาขาธนาคารกรุงไทยทั่วประเทศ ในอัตรา 10 บาท<br />
-                2. ATM ธนาคารกรุงไทย ในอัตรา 5 บาท<br />
-                - กรณีมีเหตุขัดข้องไม่สามารถชำระเงินได้ กรุณาติดต่อ Call Center บมจ. ธนาคารกรุงไทย โทร 0-2111-1111<br />
-                - หากเกินกำหนดวันชำระเงิน ให้นักเรียนติดต่อที่ฝ่ายการเงินของโรงเรียน
-            </p>
-        </div>
-        <div style="padding-left: -40px; float: right">
-            <table class="table table-bordered table-sm table-collapse">
-                <tr>
-                    <th style="line-height: 1.5;padding-top: 5px;margin-bottom: -40px;border: 1px solid;">
-                        <h3>สำหรับเจ้าหน้าที่ธนาคาร</h3>
-                        (ลงชื่อ).................................................ผู้รับเงิน<br />
-                        วันที่....................................................<br />
-                        <p style="font-size: 16px">
-                            ลงลายมือชื่อและประทับตรา
-                        </p>
-                    </th>
-                </tr>
-            </table>
-        </div>
-    </div>
-    <hr style="height: 2px; border-width: 0; margin-top: -5px; margin-bottom: 5px"/>
-    <p style="float: right;width: 22%;padding: 5px;font-weight: bold;margin-bottom: -50px;margin-top: 10px;text-align: center;border-width: 1px;border-style: solid;">
-        ส่วนที่ 2 (สำหรับธนาคาร)
-    </p>
-    <div class="row" style="margin-left: 0px">
-        <div class="column3">
-            <img src="img/nikomwitthaya.png" alt="School icon" width="auto" height="80"/>
-            <img src="img/Krungthai.png" alt="Bank icon" width="auto" height="60" style="padding-left: 10px; padding-top: 10px"/>
-        </div>
-        <div class="column4" style="padding-top: -10px; margin-left: -15px">
-            <h5 style="line-height: 22px">
-                <b>แบบฟอร์มชำระเงินเพื่อบำรุงการศึกษา (Pay-in Slip)<br />
-                    โรงเรียนนิคมวิทยา</b>
-            </h5>
-            <table class="table table-bordered table-sm table-collapse">
-                <thead>
-                <tr>
-                    <th style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;border: 1px solid;">
-                        ธนาคารกรุงไทย จำกัด (มหาชน)
-                    </th>
-                    <th colspan="2" style="width: 50px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;border-right: 0px;">
-                        Product Code :<br />
-                        ชื่อ-นามสกุลนักเรียน :<br />
-                        รหัสประจำตัวนักเรียน (Ref.1) :<br />
-                        ชั้นห้องเรียน (Ref.2) :
-                    </th>
-                    <th style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;border-left: 0px;">
-                        <?php echo "80771" ?><br />
-                        <?php echo "เด็กชายกิตติธัช สนืทราษฎร์" ?><br />
-                        <?php echo "13980" ?><br />
-                        <?php echo "101" ?>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
-                        จำนวนเงิน (ตัวเลข)
-                    </td>
-                    <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
-                        <b>2,000.00</b>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
-                        จำนวนเงิน (ตัวอักษร)
-                    </td>
-                    <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
-                        <b>สองพันบาทถ้วน</b>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;height: 50px;">
-                        สำหรับเจ้าหน้าที่ธนาคาร
-                    </td>
-                    <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;height: 50px;">
-
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4" style="background-color: #2dfeff; border: 1px solid">
-                        <h5>
-                            กำหนดชำระที่ธนาคารกรุงไทยทุกสาขา 13 พฤศจิกายน - 1 ธันวาคม 2566
-                        </h5>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <?php $qr = '099400026587500-13980-101-200000';
-        $codabar1 = '099400026587500-13980-101-200000';
-        ?>
-        <div style="margin-top: 10px">
-            <div class="column3" style="margin-left: 40px">
-                <barcode code="<?=$qr?>" type="QR" size="0.8" error="M" disableborder="1"/>
+            <div class="column3">
+                <img src="img/nikomwitthaya.png" alt="School icon" width="auto" height="80" />
             </div>
-            <div class="barcodecell">
-                <barcode code="<?=$codabar1?>" type="CODABAR" class="barcode" height="1.5"/>
-                <div style="text-align: left">
-                    <p style="font-size: 16px"><?php  echo $codabar1 ?></p>
+
+            <div class="column4" style="padding-top: 5px">
+                <h5 style="line-height: 22px">
+                    <b>ใบแจ้งการชำระเงินเพื่อบำรุงการศึกษา<br />
+                        โรงเรียนนิคมวิทยา</b>
+                </h5>
+                <hr style="height: 2px;border-width: 0;margin-top: -7px;margin-bottom: 5px;" />
+                <b>
+                    ภาคเรียนที่ <?php echo "2" ?>
+                    ปีการศึกษา <?php echo "2566" ?></b>
+            </div>
+        </div>
+
+        <p style="float: right;width: 22%;padding: 5px;font-weight: bold;margin-top: -80px;text-align: center;border-width: 1px;border-style: solid;">
+            ส่วนที่ 1 (สำหรับนักเรียน)
+        </p>
+        <div class="row" style="margin-left: 0px; margin-right: 0px">
+            <div style="margin-left: 0px;margin-right: 0px;margin-top: 10px;float: left;width: 20%;">
+                ชื่อ-นามสกุลนักเรียน<br />
+                รหัสประจำตัวนักเรียน<br />
+                ชั้นห้องเรียน
+            </div>
+            <div>
+                <b>
+                    <?php echo "เด็กชายกิตติธัช สนืทราษฎร์" ?><br />
+                    <?php echo "13980" ?><br />
+                    <?php echo "ม.1/1" ?><br /></b>
+            </div>
+
+            <div style="margin-top: 5px">
+                <table class="table table-bordered table-sm table-collapse" style="border: 1px solid">
+                    <thead>
+                        <tr>
+                            <th style="width: 450px;padding-top: 0px;vertical-align: middle;padding-bottom: 0px;background-color: #2dfeff;border: 1px solid;">
+                                <b>รายการ</b>
+                            </th>
+                            <th style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;background-color: #2dfeff;border: 1px solid;">
+                                <b>จำนวนเงิน</b>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="width: 450px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
+                                1. ค่าบำรุงการศึกษา
+                            </td>
+                            <td style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
+                                2,000.00
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 450px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;border: 1px solid;">
+                                <b>( สองพันบาทถ้วน )</b>
+                            </td>
+                            <td style="width: 150px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;background-color: #2dfeff;border: 1px solid;">
+                                <b>2,000.00</b>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="column5">
+                <h3>หมายเหตุ</h3>
+                <p style="font-size: 16px">
+                    ช่องทางการชำระเงิน ดังนี้<br />
+                    1. สาขาธนาคารกรุงไทยทั่วประเทศ ในอัตรา 10 บาท<br />
+                    2. ATM ธนาคารกรุงไทย ในอัตรา 5 บาท<br />
+                    - กรณีมีเหตุขัดข้องไม่สามารถชำระเงินได้ กรุณาติดต่อ Call Center บมจ. ธนาคารกรุงไทย โทร 0-2111-1111<br />
+                    - หากเกินกำหนดวันชำระเงิน ให้นักเรียนติดต่อที่ฝ่ายการเงินของโรงเรียน
+                </p>
+            </div>
+            <div style="padding-left: -40px; float: right">
+                <table class="table table-bordered table-sm table-collapse">
+                    <tr>
+                        <th style="line-height: 1.5;padding-top: 5px;margin-bottom: -40px;border: 1px solid;">
+                            <h3>สำหรับเจ้าหน้าที่ธนาคาร</h3>
+                            (ลงชื่อ).................................................ผู้รับเงิน<br />
+                            วันที่....................................................<br />
+                            <p style="font-size: 16px">
+                                ลงลายมือชื่อและประทับตรา
+                            </p>
+                        </th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <hr style="height: 2px; border-width: 0; margin-top: -5px; margin-bottom: 5px" />
+        <p style="float: right;width: 22%;padding: 5px;font-weight: bold;margin-bottom: -50px;margin-top: 10px;text-align: center;border-width: 1px;border-style: solid;">
+            ส่วนที่ 2 (สำหรับธนาคาร)
+        </p>
+        <div class="row" style="margin-left: 0px">
+            <div class="column3">
+                <img src="img/nikomwitthaya.png" alt="School icon" width="auto" height="80" />
+                <img src="img/Krungthai.png" alt="Bank icon" width="auto" height="60" style="padding-left: 10px; padding-top: 10px" />
+            </div>
+            <div class="column4" style="padding-top: -10px; margin-left: -15px">
+                <h5 style="line-height: 22px">
+                    <b>แบบฟอร์มชำระเงินเพื่อบำรุงการศึกษา (Pay-in Slip)<br />
+                        โรงเรียนนิคมวิทยา</b>
+                </h5>
+                <table class="table table-bordered table-sm table-collapse">
+                    <thead>
+                        <tr>
+                            <th style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;border: 1px solid;">
+                                ธนาคารกรุงไทย จำกัด (มหาชน)
+                            </th>
+                            <th colspan="2" style="width: 50px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;border-right: 0px;">
+                                Product Code :<br />
+                                ชื่อ-นามสกุลนักเรียน :<br />
+                                รหัสประจำตัวนักเรียน (Ref.1) :<br />
+                                ชั้นห้องเรียน (Ref.2) :
+                            </th>
+                            <th style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;border-left: 0px;">
+                                <?php echo "80771" ?><br />
+                                <?php echo "เด็กชายกิตติธัช สนืทราษฎร์" ?><br />
+                                <?php echo "13980" ?><br />
+                                <?php echo "101" ?>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
+                                จำนวนเงิน (ตัวเลข)
+                            </td>
+                            <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
+                                <b>2,000.00</b>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;">
+                                จำนวนเงิน (ตัวอักษร)
+                            </td>
+                            <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: left;border: 1px solid;">
+                                <b>สองพันบาทถ้วน</b>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 200px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;height: 50px;">
+                                สำหรับเจ้าหน้าที่ธนาคาร
+                            </td>
+                            <td colspan="3" style="width: 300px;padding-top: 0px;padding-bottom: 0px;vertical-align: middle;text-align: right;border: 1px solid;height: 50px;">
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="4" style="background-color: #2dfeff; border: 1px solid">
+                                <h5>
+                                    กำหนดชำระที่ธนาคารกรุงไทยทุกสาขา 13 พฤศจิกายน - 1 ธันวาคม 2566
+                                </h5>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div style="margin-top: 10px">
+                <div class="column3" style="margin-left: 40px">
+                    <barcode code="<?php echo $data['qr'] ?>" type="QR" size="0.8" error="M" disableborder="1" />
+                </div>
+                <div class="barcodecell">
+                    <barcode code="<?php echo $data['codabar1'] ?>" type=" CODABAR" class="barcode" height="1.5" />
+                    <div style="text-align: left">
+                        <p style="font-size: 16px"><?php echo $data['codabar1'] ?></p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
