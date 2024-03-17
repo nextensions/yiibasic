c79fd1c2f83eef50fc81a62213df139d9de44e6b
NSjunior <junior.sw.nextschool@gmail.com>
2024-03-17 22:19:26 +0700

    add link work and link tree in the main page

--- views/site/index.php
+++ views/site/index.php
@@ -1,9 +1,10 @@
 <?php
-
+use yii\helpers\Html;
 /** @var yii\web\View $this */
 
 $this->title = 'My Yii Application';
 ?>
+<!--
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
@@ -51,3 +52,44 @@ $this->title = 'My Yii Application';
 
     </div>
 </div>
+-->
+<br/>
+<br/>
+<div class="row">
+<div class="col-lg-4 mb-3">
+            <h2>In product</h2>
+<?php echo Html::a('พระนารายณ์ ใบมอบตัว', ['/paper/profile_phanarai'], ['class' => 'btn btn-success']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4265">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('ไตรมิตร ใบมอบตัว', ['/paper/profile_traimit'], ['class' => 'btn btn-success']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4248">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('พุทไธสง ปก ปพ.5', ['/paper/putthaisong_transcript5'], ['class' => 'btn btn-success']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4264">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('นิคม บัตรประจำตัวผู้เข้าสอบ', ['/paper/examidcard'], ['class' => 'btn btn-success']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4294">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('จินดาพร แก้ไขรายงานการรับเงิน', ['/report/receipt'], ['class' => 'btn btn-success']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4209">Link Tree &raquo;</a>
+</div>
+    <div class="col-lg-4 mb-3">
+        <h2>In pull request</h2>
+            <?php echo Html::a('แบบเยี่ยมบ้านบดิน (แบบบันทึกเยี่ยมบ้าน)', ['/paper/visit_bodin'], ['class' => 'btn btn-warning']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4342">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('แบบเยี่ยมบ้านบดิน (แบบสรุปเยี่ยมบ้าน)', ['/paper/visit_bodin_summary'], ['class' => 'btn btn-warning']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4342">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('พุทไธสง คะแนน ปพ.5 ตั้ง margin', ['/paper/putthaisong_transcript_eva'], ['class' => 'btn btn-warning']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4351">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('พุทไธสง เข้าเรียน ปพ.5 ตั้ง margin', ['/paper/putthaisong_transcript_attendance'], ['class' => 'btn btn-warning']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4351">Link Tree &raquo;</a>
+<br/><br/>
+<?php echo Html::a('Image cropper', ['/image/imagecrop'], ['class' => 'btn btn-warning']); ?>&nbsp;
+<a class="btn btn-outline-secondary" href="https://tree.nextschool.io/project/mai-nextschool/issue/4050">Link Tree &raquo;</a>
+</div>
+</div>
+
+
